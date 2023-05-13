1f015ed28e515868b51a560d4b9d2f964ed6de28
Rahulgautama <rahul8874950727@gmail.com>
2023-05-13 22:58:28 +0530

    TC 2.2 Find 3 Float in maximum number

--- FindMaximumProblemGeneric/Program.cs
+++ FindMaximumProblemGeneric/Program.cs
@@ -7,7 +7,7 @@ namespace FindMaximumProblemGeneric
         static void Main(string[] args)
         {
             float firstNumber = 180.1f;
-            float secondNumber = 160.2f;
+            float secondNumber = 180.2f;
             float thirdNumber = 170.3f;
             FindMaximumFloatNumber findMaximumIntegerNumber = new FindMaximumFloatNumber();
             float maxNumber=findMaximumIntegerNumber.FindMaxIntegerNumber(firstNumber,secondNumber,thirdNumber);
