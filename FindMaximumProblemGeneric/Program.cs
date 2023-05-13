0130a342942e8fba2ef3e5f59f5bf24011e6ab95
Rahulgautama <rahul8874950727@gmail.com>
2023-05-13 22:37:25 +0530

    Find 3 integer in maximum number

---
+++ FindMaximumProblemGeneric/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindMaximumProblemGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 120;
+            int secondNumber = 167;
+            int thirdNumber = 144;
+            FindMaximumIntegerNumber findMaximumIntegerNumber = new FindMaximumIntegerNumber();
+            int maxNumber=findMaximumIntegerNumber.FindMaxIntegerNumber(firstNumber,secondNumber,thirdNumber);
+
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
